82f68e7c5f9b18b1265551663ebc4d9ccf9cd045
root <root@vps-36d9dbdd.vps.ovh.net>
2021-01-19 15:38:15 +0000

    C2. Revisión

--- main.sh
+++ main.sh
@@ -1,2 +1,3 @@
 #!/bin/bash 
-echo "Hola Mundo Jenkins soy un script de Linux"
+echo "Hola Mundo Jenkins soy un script de Linux. Francisco Javier Melero"
+echo "Para practicar tareas sencillas en Jenkins, sin build y sin test, y sin deploy nube"
