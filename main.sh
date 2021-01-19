48a306a52020918f57713cbecaed958e85f50fc9
Francisco Javier Melero <franciscomelero@politecnicomalaga.com>
2021-01-19 16:16:22 +0000

    C5. Nueva Revisión

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo "Hola Mundo Jenkins soy un script de Linux. Francisco Javier Melero"
 echo "Para practicar tareas sencillas en Jenkins, sin build y sin test, y sin deploy nube"
-echo "Podremos automatizar, manual, hook con github (push)
+echo "Podremos automatizar, manual, hook con github (push)"
