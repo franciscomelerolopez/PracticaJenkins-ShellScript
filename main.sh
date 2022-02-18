12b6fe3f642945e3bec1ef4daff574375bb1a873
Francisco Javier Melero López <66321313+franciscomelerolopez@users.noreply.github.com>
2022-02-18 08:40:11 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 echo "Hola Mundo Jenkins soy un script de Linux. Francisco Javier Melero"
 echo "Para practicar tareas sencillas en Jenkins, sin build y sin test, y sin deploy nube"
 echo "Podremos automatizar, manual, hook con github (push)"
-echo "OJORGE......dfsafaffsdff............................................ao"
+echo "Vamos"
